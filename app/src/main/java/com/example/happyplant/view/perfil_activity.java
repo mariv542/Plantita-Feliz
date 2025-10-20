c657d1662cd23674737fff341c857606b3bd54eb
mariv542 <marielvy542@gmail.com>
2025-10-20 16:40:06 -0300

    Primer intento de colores

--- app/src/main/java/com/example/happyplant/view/perfil_activity.java
+++ app/src/main/java/com/example/happyplant/view/perfil_activity.java
@@ -1,11 +1,14 @@
 package com.example.happyplant.view;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.ImageButton;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import com.example.happyplant.R;
 import com.example.happyplant.utils.GPSHelper;
@@ -17,6 +20,9 @@ public class perfil_activity extends AppCompatActivity {
     private ImageButton btnPerfil_regresar;
     private TextView txtGPS;
     private GPSHelper gpsHelper;
+    private Switch switchModoNegro;
+    private SharedPreferences sharedPreferences;
+
     @Override
     protected void onCreate(Bundle saveInstanceState) {
         super.onCreate(saveInstanceState);
@@ -30,7 +36,35 @@ public class perfil_activity extends AppCompatActivity {
         // card informacion personal
         card_InformacionPersonal = findViewById(R.id.card_perfil_informacionPersonal);
 
+        // 1 inicializar SharedPreferences
+        sharedPreferences = getSharedPreferences("AppPrefs", MODE_PRIVATE);
+        boolean isDarkMode = sharedPreferences.getBoolean("dark_mode", false);
+
+        // 2 Aplicar modo guardado
+        AppCompatDelegate.setDefaultNightMode(
+                isDarkMode ? AppCompatDelegate.MODE_NIGHT_YES : AppCompatDelegate.MODE_NIGHT_NO
+        );
+
+        // 3 Referencia al Switch
+        switchModoNegro = findViewById(R.id.switchModoNegro);
+        switchModoNegro.setChecked(isDarkMode);
         //+--------------------------------------------------------------------------------------------+
+
+        // 4. Escuchar cambios del Switch
+        switchModoNegro.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            // Guardar preferencia
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putBoolean("dark_mode", isChecked);
+            editor.apply();
+
+            // Cambiar modo oscuro/claro
+            AppCompatDelegate.setDefaultNightMode(
+                    isChecked ? AppCompatDelegate.MODE_NIGHT_YES : AppCompatDelegate.MODE_NIGHT_NO
+            );
+
+            // Opcional reiniciar la actividad para aplicar cambios inmediatos
+            recreate();
+        });
 
         gpsHelper = new GPSHelper(this);
         gpsHelper.obtenerUbicacion((lat, lon) -> {
